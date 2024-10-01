6e8cb78c184a960c5c41a3dde7f2d49abfb2a055
JovanaPetrovic12 <166700932+JovanaPetrovic12@users.noreply.github.com>
2024-10-01 10:29:11 +0200

    added all files commit to main

---
+++ docs/index.6c721491.js
@@ -0,0 +1,2 @@
+console.log("hggjh"),document.querySelector("h1").style.color="hotpink";
+//# sourceMappingURL=index.6c721491.js.map
---
+++ src/modules/main.js
@@ -0,0 +1,4 @@
+console.log('hggjh');
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'hotpink';
